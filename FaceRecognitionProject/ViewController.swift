1b3de39eb00f56db57f999f29efbccc528979fda
Bilgehan KAYA <bilgehankaya79@gmail.com>
2021-05-20 03:04:27 +0300

    App Finished

--- FaceRecognitionProject/ViewController.swift
+++ FaceRecognitionProject/ViewController.swift
@@ -1,19 +1,41 @@
-//
-//  ViewController.swift
-//  FaceRecognitionProject
-//
-//  Created by Bilgehan KAYA on 20.05.2021.
-//
-
 import UIKit
+import LocalAuthentication
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myLabel: UILabel!
+    
+    
+    ///info.plish içerisine aşağıdaki satırı ekliyoruz
+    //Privacy - Face ID Usage Description
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    @IBAction func signInClicked(_ sender: Any) {
+        let authContext = LAContext()
+        
+        var error: NSError?
+        
+        if authContext.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+            
+            authContext.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: "Barnağını bas hacı.") { success, error in
+                if success == true {
+                    // successful auth
+                    
+                    //işlemin ana thread de çalışması için aşağıdaki kody yazıyoruz
+                    DispatchQueue.main.async {
+                        self.performSegue(withIdentifier: "toSecondVC", sender: nil)
+                    }
+                    
+                }
+                else {
+                    DispatchQueue.main.async {
+                        self.myLabel.text = "Error!"
+                    }
+                }
+            }
+            
+        }
+    }
 }
 
